dc31241971bfec0a3362fd67a2a99c74ab4a4f6f
elissarf <elissar.fadel@dawsoncollege.qc.ca>
2024-02-23 23:54:50 -0500

    added csv file reader function and function to get all data from csv to seed

---
+++ server/utils/data-init.js
@@ -0,0 +1,47 @@
+const CsvReadableStream = require('csv-reader');
+const fs = require('fs');
+
+
+async function getAllLeases(filePath = 'data/House_Rent_Dataset.csv') {
+    const data = await readCsvFile(filePath);
+    return data;
+}
+async function readCsvFile(filePath) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        const inputStream = fs.createReadStream(filePath, 'utf8');
+        let isFirstRow = true;
+
+        inputStream.
+            pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true })).
+            on('data', (row) => {
+                if (isFirstRow) {
+                    isFirstRow = false;
+                } else {
+                    const customizedObject = {
+                        'postedDate': row[0], 
+                        'bhk': row[1],
+                        'rentPrice': row[2],
+                        'size': row[3],
+                        'floor': row[4],
+                        'areaType': row[5],
+                        'areaLocality': row[6],
+                        'city': row[7],
+                        'furnishing': row[8],
+                        'preferredTentant': row[9],
+                        'bathroom': row[10],
+                        'pointOfContact': row[11]
+                    };
+                    results.push(customizedObject);
+                }
+            }).
+            on('end', () => {
+                resolve(results);
+            }).
+            on('error', (error) => {
+                reject(error);
+            });
+    });
+}
+
+module.exports = {getAllLeases};
--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,6 +1,5 @@
 const { DB } = require('../db/db.js');
-// const {getAllLeases} = require('./data-init.js');
-// const {Release} = require('../db/schemas.js');
+const {getAllLeases} = require('./data-init.js');
 
 process.env.DEBUG = 'server';
 const debug = require('debug');
@@ -8,34 +7,22 @@ const logger = debug('server');
 
 
 (async () => {
-  try {
-    const db = new DB();
+    try {
+        const db = new DB();
 
-    // const datas = await getAllLeases();
-    const data = [{
-        'posted_on': 'String1', 
-        'bhk': 1,
-        'rent': 1,
-        'size': 1,
-        'floor': 'String2',
-        'area_type': 'String3',
-        'area_local': 'String4',
-        'city': 'String',
-        'furnishing': 'String',
-        'bathroom': 1
-    }];
-    await db.createManyLeases(data);
-    logger('seeded database');
+        const data = await getAllLeases();
+        await db.createManyLeases(data);
+        logger('seeded database');
 
 
-    // Close the database connection (if your DB class supports this)
-    await db.close();
+        // Close the database connection (if your DB class supports this)
+        await db.close();
 
-    // Exit the program with a success code (0)
-    process.exit(0);
-  } catch (e) {
-    console.error('Could not connect or insert data:', e);
-    // Exit the program with an error code (1)
-    process.exit(1);
-  }
+        // Exit the program with a success code (0)
+        process.exit(0);
+    } catch (e) {
+        console.error('Could not connect or insert data:', e);
+        // Exit the program with an error code (1)
+        process.exit(1);
+    }
 })();
