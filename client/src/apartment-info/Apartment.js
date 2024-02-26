bb04c521fe6b37df20b543bcc7905f2a5ee858c0
Belaru <nactiabond.ota@gmail.com>
2024-02-26 09:52:07 -0500

    Add appartment info component

---
+++ client/src/apartment-info/Apartment.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './Appartment.css';
+
+/**
+ * Home component for displaying data related to a selected country.
+ *
+ * @component
+ * @returns {JSX.Element} Rendered Home component.
+ */
+export default function ApartmentPage(apartment) {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
