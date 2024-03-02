a9e097a38e813ed860e59d03d0c8a641c2810f65
Monica Dimitrova <monica.dimitrova@dawsoncollege.qc.ca>
2024-03-02 15:00:36 -0500

    Adding information for apt description

--- client/src/apartmentDisplay/Apartment.js
+++ client/src/apartmentDisplay/Apartment.js
@@ -14,8 +14,26 @@ export default function ApartmentPage() {
     const apartment = location.state?.apartment;
     return (
         <div>
-            <ApartmentImages imagesLinks={apartment.images}/>
+            {/* <ApartmentImages imagesLinks={apartment.images}/> */}
             <div id="apartment-information">
+                <h1>infomationnnnn</h1>
+                <ApartmentImages imagesLinks={apartment.images}/>
+                <h3>
+                    {apartment.furnishing} apartment located in {apartment.areaLocality}, {apartment.city}
+                </h3>
+                <h4>
+                    Rent price: {apartment.rentPrice}
+                    Floor: {apartment.floor}
+                    Size: {apartment.size}
+                    Bedrooms: {apartment.bhk}
+                    Bathrooms: {apartment.bathroom}
+                    Furnishing: {apartment.furnishing}
+                    Listing post date: {apartment.postedDate}
+                </h4>
+                <h3>
+                    For more information, contact the lister.
+                </h3>
+                <button>Interested</button>
                 <ContactSection/>
             </div>
         </div>
