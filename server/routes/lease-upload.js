177642b5aef22d8a3cb1ed63e44ee194d3544dae
elissarf <elissar.fadel@dawsoncollege.qc.ca>
2024-03-02 23:34:20 -0500

    added validation for json object for postlisting api

--- server/routes/lease-upload.js
+++ server/routes/lease-upload.js
@@ -3,12 +3,37 @@ const router = express.Router();
 const {DB} = require('../db/db');
 
 router.post('/', async (req, res) => {
-    console.log(req.body);
-    res.status(200).send({'ok'});
+    const property = req.body;
+    console.log(property);
+
+    if (!validateProperty(property)) {
+        // If validation fails, send a bad request response
+        return res.status(400).json({ error: 
+            'Validation failed. Please provide valid property details.' });
+    }
+    res.status(200).send({'respose':'ok'});
 
 });
 
 
+// Validation function
+function validateProperty(property) {
+    const { title, rentPrice, address, 
+        contactInfo, size, 
+        bedrooms, bathrooms, floorNumber, furnishing } = property;
+    // Check if all required fields are present and non-empty
+    if (!title || !rentPrice || !address 
+        || !contactInfo 
+        || !size || !bedrooms || !bathrooms || !floorNumber || !furnishing) {
+        return false;
+    }
+    // Check if rentPrice, size, bedrooms, bathrooms, and floorNumber are numbers
+    if (isNaN(rentPrice) || isNaN(size) 
+    || isNaN(bedrooms) || isNaN(bathrooms) || isNaN(floorNumber)) {
+        return false;
+    }
+    return true;
+}
 
 module.exports = {
     leaseUploadRouter: router
