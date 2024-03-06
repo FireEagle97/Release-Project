290a3e1585dff743773dc6f7c7c800f72eac337d
elissarf <elissar.fadel@dawsoncollege.qc.ca>
2024-03-06 11:20:46 -0500

    fixed more footer styling from code review

--- client/src/footer/FooterPlace.js
+++ client/src/footer/FooterPlace.js
@@ -11,39 +11,43 @@ function Footer(){
 
     return <>
         <footer>
-          <div class="top">
-            <div class="left">
-              <p><strong>Credits for ReLease 2024:</strong></p>
-              <p>Elissar Fadel</p>
-              <p>Monica Dimitrova</p>
-              <p>Anastasia Bondarenko</p>
-              <p>Dany Makhoul</p>
+          
+            <div class="top">
+              <div class="logo-footer">
+                <picture><img src="/logo1.png" alt="Logo" class="image-footer" /></picture>
+              </div>
+              <div class="left">
+                <p><strong>Credits for ReLease 2024:</strong></p>
+                <p>Elissar Fadel</p>
+                <p>Monica Dimitrova</p>
+                <p>Anastasia Bondarenko</p>
+                <p>Dany Makhoul</p>
+              </div>
+              <div className="right">
+                <p><strong>Quick links:</strong></p>
+                <ul className="footer-links">
+                  <li>
+                    <Link to="/" onClick={scrollToTop}>Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about" onClick={scrollToTop}>About</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact" onClick={scrollToTop}>Contact</Link>
+                  </li>
+                  <li>
+                    <Link to="/post-listing" onClick={scrollToTop}>Post Listing</Link>
+                  </li>
+                  <li>
+                    <Link to="/apartments" onClick={scrollToTop}>View Apartments</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="right">
-              <p><strong>Quick links:</strong></p>
-              <ul className="footer-links">
-                <li>
-                  <Link to="/" onClick={scrollToTop}>Home</Link>
-                </li>
-                <li>
-                  <Link to="/about" onClick={scrollToTop}>About</Link>
-                </li>
-                <li>
-                  <Link to="/contact" onClick={scrollToTop}>Contact</Link>
-                </li>
-                <li>
-                  <Link to="/post-listing" onClick={scrollToTop}>Post Listing</Link>
-                </li>
-                <li>
-                  <Link to="/apartments" onClick={scrollToTop}>View Apartments</Link>
-                </li>
-              </ul>
+          
+            <div class="policy">
+              <p>Policy: We are not responsible for any leases posted by users. If you find a post inappropriate, feel free to report it to us.</p>
             </div>
-          </div>
-        
-        <div class="policy">
-          <p>Policy: We are not responsible for any leases posted by users. If you find a post inappropriate, feel free to report it to us.</p>
-        </div>
       </footer>
     </>
 }
