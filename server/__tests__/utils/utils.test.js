5a4459744cefa7991c04781096303b36aaa916a5
elissarf <elissar.fadel@dawsoncollege.qc.ca>
2024-03-13 11:06:20 -0400

    added beforeeach and aftereach for random mocking

--- server/__tests__/utils/utils.test.js
+++ server/__tests__/utils/utils.test.js
@@ -1,24 +1,21 @@
 const {getRandomNumber, getRandomAddressCityPair, 
     getRandomDate, getRandomPrice} = require('../../utils/utils');
 
-// beforeEach(() => {
-//     jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-// });
+beforeEach(() => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+});
 
-// afterEach(() => {
-//     jest.spyOn(global.Math, 'random').mockRestore();
-// });
+afterEach(() => {
+    jest.spyOn(global.Math, 'random').mockRestore();
+});
 
 describe('getRandomNumber', () => {
     
     test('returns a random number within the specified range', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
 
         const min = 1;
         const max = 10;
         expect(getRandomNumber(min, max)).toBe(6); 
-
-        jest.spyOn(global.Math, 'random').mockRestore();
     });
 });
 
@@ -28,7 +25,7 @@ describe('getRandomAddressCityPair', () => {
         const mockGetRandomNumber = jest.fn(() => 0);
   
         const result = getRandomAddressCityPair(mockGetRandomNumber);
-  
+
         expect(result).toEqual({ city: 'Toronto', 
             address: '411 Castlefield Ave, Toronto, ON M5N 1L4' });
     });
@@ -36,61 +33,30 @@ describe('getRandomAddressCityPair', () => {
 
 describe('getRandomDate', () => {
     test('returns a random date between 2024-01-01 and current date', () => {
-  
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-  
         const result = getRandomDate();
-  
         expect(result).toBe('2024-02-06'); 
-  
-        jest.restoreAllMocks();
     });
 });
 
 describe('getRandomPrice', () => {
     test('returns a random price for 1 BHK', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-
         const result = getRandomPrice(1);
-
         expect(result).toBe(1150); 
-
-        jest.restoreAllMocks();
     });
     test('returns a random price for 2 BHK', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-
         const result = getRandomPrice(2);
-
         expect(result).toBe(2000); 
-
-        jest.restoreAllMocks();
     });
     test('returns a random price for 3 BHK', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-
         const result = getRandomPrice(3);
-
         expect(result).toBe(3150); 
-
-        jest.restoreAllMocks();
     });
     test('returns a random price for 4 BHK', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-
         const result = getRandomPrice(4);
-
         expect(result).toBe(4600); 
-
-        jest.restoreAllMocks();
     });
     test('returns a random price for 5 BHK, aka null price', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-
         const result = getRandomPrice(5);
-
         expect(result).toBe(null); 
-
-        jest.restoreAllMocks();
     });
 });
