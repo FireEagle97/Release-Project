54c17266edbf91ae56bff6ffb636cb05a7010a4e
elissarf <elissar.fadel@dawsoncollege.qc.ca>
2024-03-19 23:16:43 -0400

    added more validation

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {DB} = require('../db/db');
 
+const idRegex = /^[a-f\d]{24}$|^\d{1,12}$|^(?:(?:[0-9a-fA-F]{2}){12})$/;
+
 router.post('/:leaseId', async (req, res) => {
     const leaseId = req.params.leaseId;
 
+    if (!idRegex.test(leaseId)) {
+        return res.status(400).json({ error: 'Invalid ID format' });
+    }
     try {
         // Fetch the lease from the database
         const db = new DB();
