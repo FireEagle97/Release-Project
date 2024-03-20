ead3ebf1e454bcc4adbdff9102c907ea6bf797e0
elissarf <elissar.fadel@dawsoncollege.qc.ca>
2024-03-20 18:24:16 -0400

    added testing for upoading image in uplaod lease api

--- server/__tests__/lease-upload-utils.test.js
+++ server/__tests__/lease-upload-utils.test.js
@@ -1,4 +1,6 @@
 const { getCurrentDate, validateProperty } = require('../routes/utils/lease-upload-utils');
+const { getImageUrls } = require('../routes/utils/image-upload');
+const { BlobServiceClient } = require('@azure/storage-blob');
 
 describe('getCurrentDate', () => {
     test('should return current date in yyyy-mm-dd format', () => {
@@ -65,5 +67,61 @@ describe('validateProperty', () => {
             furnishing: 'Furnished'
         };
         expect(validateProperty(property)).toBe(false);
+    });
+});
+
+// Mock environment variables
+process.env.AZURE_CONNECTION = 'mock_connection_string';
+process.env.AZURE_CONTAINER_NAME = 'mock_container_name';
+
+// Mock Azure BlobServiceClient methods
+jest.mock('@azure/storage-blob', () => ({
+    BlobServiceClient: {
+        fromConnectionString: jest.fn(),
+    },
+}));
+
+describe('getImageUrls', () => {
+    test('should return empty array if no files are provided', async () => {
+        const files = {};
+        const imageUrls = await getImageUrls(files);
+        expect(imageUrls).toEqual([]);
+    });
+
+    test('should return array of image URLs', async () => {
+        // Mock BlobServiceClient methods
+        const mockUploadData = jest.fn();
+        const mockBlockBlobClient = {
+            uploadData: mockUploadData,
+            url: 'mock_image_url',
+        };
+        const mockGetBlockBlobClient = jest.fn().mockReturnValue(mockBlockBlobClient);
+        BlobServiceClient.fromConnectionString.mockReturnValue({
+            getContainerClient: jest.fn().mockReturnValue({
+                getBlockBlobClient: mockGetBlockBlobClient,
+            }),
+        });
+
+        // Mock files
+        const files = {
+            file1: {
+                name: 'mock_image1.jpg',
+                mimetype: 'image/jpeg',
+                data: 'mock_image_data1',
+            },
+            file2: {
+                name: 'mock_image2.jpg',
+                mimetype: 'image/jpeg',
+                data: 'mock_image_data2',
+            },
+        };
+
+        const imageUrls = await getImageUrls(files);
+
+        // Expect BlobServiceClient methods to be called
+        expect(mockUploadData).toHaveBeenCalledTimes(2);
+
+        // Expect array of image URLs to be returned
+        expect(imageUrls).toEqual(['mock_image_url', 'mock_image_url']);
     });
 });
