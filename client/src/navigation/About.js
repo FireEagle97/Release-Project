eddecfa855190a6111e016b1d2084a0050b51e2c
Monica Dimitrova <monica.dimitrova@dawsoncollege.qc.ca>
2024-04-02 20:08:42 -0400

    Add translation for about us page

--- client/src/navigation/About.js
+++ client/src/navigation/About.js
@@ -1,15 +1,15 @@
 import './About.css';
+import { useTranslation } from 'react-i18next';
 
 export default function About() {
+    const { t } = useTranslation();
+
     return(
         <div className="about">
-            <h1>About Us</h1>
+            <h1>{t('About.aboutus')}</h1>
             <br></br>
             <p>
-                Our platform serves as a bridge between tenants looking to vacate their current rental
-                properties and people searching for the perfect place to call home. Whether you want to relocate
-                for a job, upgrade to a larger space, or downsize to a cozier home, ReLease is here to make the
-                transition smooth and hassle-free.
+                {t('About.text')}
             </p>
             <p>-Monica, Anastasia, Elissar, Dany</p>
         </div>
