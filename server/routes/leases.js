0371ee48b6b5ca96eee158d71df738f2176d11ba
Belaru <nactiabond.ota@gmail.com>
2024-04-06 10:15:46 -0400

    Add swagger documentation for leases

--- server/routes/leases.js
+++ server/routes/leases.js
@@ -14,7 +14,111 @@ router.get('/', async (req, res) => {
     }
 });
 
-//rent, size, furnishing
+
+/**
+ * @swagger
+ * /leases/{city}:
+ *   get:
+ *     summary: Retrieve filtered leases.
+ *     description: Displays leases filtered by city (mandatory) and other queries.
+ *     parameters:
+ *       - in: path
+ *         name: city
+ *         required: true
+ *         description: The city location of the leases.
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: furnishing
+ *         required: false
+ *         description: The furnishing description of the apartment
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: area
+ *         required: false
+ *         description: The area location inside the city of the lease.
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: bathroom
+ *         required: false
+ *         description: The number of the bathrooms in the apartment.
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: bedroom
+ *         required: false
+ *         description: The number of the bedrooms in the apartment.
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: rentMinimum
+ *         required: false
+ *         description: The minimum rent price of the apartment.
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: rentMaximum
+ *         required: false
+ *         description: The maximum rent price of the apartment.
+ *         schema:
+ *           type: integer
+ *         name: sizeMinimum
+ *         required: false
+ *         description: The minimum size of the apartment in squarefoot.
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: rentMaximum
+ *         required: false
+ *         description: The maximum size of the apartment in squarefoot.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: The list of filtered apartments.
+ *         content:
+ *           application/json:
+ *             example:
+ *               country: Estonia, Rep. of
+ *               ISO3: EST
+ *               year: "2000"
+ *               temperature_change: "2.066"
+ *             schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 year:
+ *                   type: string
+ *                   description: temperature change in celsius
+ *                 ISO3:
+ *                   type: string
+ *                   description: country 3 letter code
+ *                 country:
+ *                   type: string
+ *                   description: country name
+ *                 temperature_change:
+ *                   type: string
+ *                   description: temperature change compared to previous year
+ *       400:
+ *         description: Invalid query paramater value input.
+ *         content:
+ *           application/json:
+ *             example:
+ *               status: error
+ *               message: Rent values must be non-negative integers
+ *             schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *                 description: rejected
+ *               message:
+ *                 type: string
+ *                 description: specifies invalid input
+ * */
 
 router.get('/:city', async (req, res) => {
     try{
